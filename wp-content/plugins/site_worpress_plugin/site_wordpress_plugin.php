81e16463a3d09a69c8eba85d7b2572bb9c194591
Rebecca Flores <Becca@Rebeccas-MacBook-Pro.local>
2018-05-09 20:03:11 -0500

    form

---
+++ wp-content/plugins/site_worpress_plugin/site_wordpress_plugin.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name:  site_wordpress_plugin
+Description:  site_wordpress_plugin
+Version:      1.0
+Author:       Rebecca Flores
+Author URI:
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:
+Domain Path:  /languages
+*/
+// remove default dashboard meta boxes
+// function nvcwp_basic_plugin() {
+//     remove_meta_box( 'dashboard_primary', 'dashboard', 'normal' );
+//     remove_meta_box( 'dashboard_activity', 'dashboard', 'normal' );
+//     remove_meta_box( 'dashboard_quick_press', 'dashboard', 'normal' );
+    // remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+//     remove_action('welcome_panel', 'wp_welcome_panel');
+// }
+//
+// add_action( 'admin_init', 'nvcwp_basic_plugin' );
